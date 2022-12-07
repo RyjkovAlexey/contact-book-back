43d2b8fbd7a28ebe93ad52eca34611d978bbe150
RyjkovAlexey <kawai4ik98@gmail.com>
2022-12-07 22:42:54 +0300

    init

---
+++ src/main/java/ru/alexey/contactbook/contactbookback/ContactBookBackApplication.java
@@ -0,0 +1,13 @@
+package ru.alexey.contactbook.contactbookback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactBookBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/alexey/contactbook/contactbookback/ContactBookBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alexey.contactbook.contactbookback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBookBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
